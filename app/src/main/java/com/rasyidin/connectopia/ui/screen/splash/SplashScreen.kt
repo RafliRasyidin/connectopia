0a7a7454f0d08529c5682196890e42c2e2636376
raflirasyidin <raflirasyidin20@gmail.com>
2024-05-14 19:54:17 +0700

    add splash screen

--- app/src/main/java/com/rasyidin/connectopia/ui/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/rasyidin/connectopia/ui/screen/splash/SplashScreen.kt
@@ -1,11 +1,29 @@
 package com.rasyidin.connectopia.ui.screen.splash
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.rasyidin.connectopia.MainActivity
+import com.rasyidin.connectopia.databinding.ActivitySplashBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class SplashScreenActivity: AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT: Long = 1500
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        lifecycleScope.launch(Dispatchers.Main) {
+            delay(SPLASH_TIME_OUT)
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
+        }
     }
 }
