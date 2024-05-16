bd7eee055dc8160f8696ffeda7de92a527896de7
raflirasyidin <raflirasyidin20@gmail.com>
2024-05-16 14:48:47 +0700

    secure API with NDK

--- app/src/main/cpp/native-lib.cpp
+++
@@ -1,8 +0,0 @@
-#include <jni.h>
-
-
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_rasyidin_connectopia_utils_Constants_webClientFirebaseAPI(JNIEnv *env, jobject thiz) {
-    return env ->NewStringUTF("935113078481-bgpr1003em1skh3apn2puph488g8ses5.apps.googleusercontent.com");
-}
