a3cfd52074051ef2fd91970ee84d32c1fb954042
raflirasyidin <raflirasyidin20@gmail.com>
2024-05-17 15:47:45 +0700

    fix login and logout with store data account to firebase

--- app/src/main/java/com/rasyidin/connectopia/ui/screen/on_board/OnBoardingViewModel.kt
+++ app/src/main/java/com/rasyidin/connectopia/ui/screen/on_board/OnBoardingViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.firestore.FirebaseFirestore
 import com.rasyidin.connectopia.model.user.UserData
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -16,13 +17,22 @@ class OnBoardingViewModel(private val db: FirebaseFirestore) : ViewModel() {
     val signInState = _signInState.asStateFlow()
 
     fun onSignInResult(result: SignInResult) {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             try {
+                if (result.data == null) {
+                    _signInState.update { signState ->
+                        signState.copy(isSignedInSuccessful = false)
+                    }
+                    return@launch
+                }
                 _signInState.update { signState ->
-                    if (result.data != null) {
+                    val userData = getUserData(result.data.userId)
+                    if (userData != null) {
+                        db.collection("users").document(userData.userId).set(userData).await()
+                    } else {
                         db.collection("users").document(result.data.userId).set(result.data).await()
                     }
-                    signState.copy(isSignedInSuccessful = result.data != null)
+                    signState.copy(isSignedInSuccessful = true)
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -31,6 +41,16 @@ class OnBoardingViewModel(private val db: FirebaseFirestore) : ViewModel() {
                 }
             }
 
+        }
+    }
+
+    private suspend fun getUserData(userId: String): UserData? {
+        try {
+            val userData = db.collection("users").document(userId).get().await().toObject(UserData::class.java)
+            return userData
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
         }
     }
 
